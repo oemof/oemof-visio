ff2f2d3b046c9f1553d8a0e1d5643d866f96d3e4
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-09-03 16:24:47 +0200

    Update setup.py
    (also lint with black)

--- setup.py
+++ setup.py
@@ -11,14 +11,16 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='oemof_visio',
-      version='v0.0.1',
-      author='oemof developing group',
-      author_email='oemof@rl-institut.de',
-      description='The open energy modelling framework',
-      url='https://oemof.org/',
-      namespace_package=['oemof_visio'],
-      long_description=read('README.rst'),
-      packages=find_packages(),
-      package_dir={'oemof_visio': 'oemof_visio'},
-      install_requires=['matplotlib', 'pandas'])
+setup(
+    name="oemof_visio",
+    version="v0.0.1",
+    author="oemof developing group",
+    author_email="oemof@rl-institut.de",
+    description="The open energy modelling framework",
+    url="https://oemof.org/",
+    namespace_package=["oemof_visio"],
+    long_description=read("README.rst"),
+    packages=find_packages(),
+    package_dir={"oemof_visio": "oemof_visio"},
+    install_requires=["matplotlib", "pandas", "graphviz", "oemof.solph"],
+)
