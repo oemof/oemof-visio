1d4cac90faf8fe9cb87b303bcbb198d71f215a42
Patrik Schönfeldt <Patrik.Schoenfeldt@dlr.de>
2025-03-28 10:44:43 +0100

    Align structure with solph

--- oemof_visio/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import plot
-from .energy_system_graph import ESGraphRenderer
-from ._version import __version__ as version
---
+++ src/oemof/visio/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.1"
+
+from . import plot
+from .energy_system_graph import ESGraphRenderer
+
+__all__ = [
+    "plot",
+    "ESGraphRenderer",
+]
